173a2285ddaeb9978eebdc318d8d24e8fbe13e53
UgurTutuncuoglu <ugurtutuncu6@gmail.com>
2025-04-25 21:37:49 +0300

    .yml file was modified for using "chrome puppeteer" not "local puppeteer"

--- smoke-test.js
+++ smoke-test.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   try {
     const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/google-chrome',
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
